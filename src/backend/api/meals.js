286aaf1dc4d605f06a48dddf489c10a781d9dbeb
Rostyslav Zhyvitskyi <rostik94@gmail.com>
2022-09-17 19:25:52 +0200

    meals routes done

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -2,14 +2,87 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../database');
 
-router.get('/', async (request, response) => {
+router.get('/', async (req, res) => {
   try {
     // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex.select('title').from('meal');
+    const meals = await knex.select().from('meal');
 
-    response.json(titles);
+    if (meals.length !== 0) {
+      res.json(meals);
+    } else {
+      res.status(404).json({ error: 'No meals found' });
+    }
   } catch (error) {
     throw error;
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const newMeal = req.body;
+
+    await knex.insert(newMeal).into('meal');
+    const meals = await knex.select().from('meal');
+
+    res.json(meals);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const mealId = req.params.id;
+    const meal = await knex.select().from('meal').where({ id: mealId });
+
+    if (meal.length !== 0) {
+      res.json(meal[0]);
+    } else {
+      res.status(404).json({ error: 'No meal found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const mealBody = req.body;
+    const mealId = req.params.id;
+
+    const meal = await knex('meal').where({ id: mealId });
+
+    if (meal.length === 0) {
+      res.status(400).json({ error: 'No meal found' });
+    }
+
+    if (Object.keys(mealBody).length !== 0) {
+      await knex('meal').where({ id: mealId }).update(mealBody);
+      const updatedMeal = await knex('meal').where({ id: mealId });
+
+      res.json(updatedMeal[0]);
+    } else {
+      res.status(400).json({ error: 'Meal has nothing to update' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const mealId = req.params.id;
+    const meal = await knex('meal').where({ id: mealId });
+
+    if (meal.length === 0) {
+      res.status(400).json({ error: 'No meal found' });
+    }
+
+    await knex('meal').where({ id: mealId }).del();
+
+    res.json({ message: 'Meal deleted' });
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
   }
 });
 
